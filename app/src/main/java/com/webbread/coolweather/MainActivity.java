df21ec0c9148df3b5dc15cd469eac0e9b89d1cdb
webbread@126.com <webbread@126.com>
2017-04-11 14:35:07 +0800

    first commit

---
+++ app/src/main/java/com/webbread/coolweather/MainActivity.java
@@ -0,0 +1,14 @@
+package com.webbread.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //http://guolin.tech/api/weather/register
+    //http://guolin.tech/api/weather/doc
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
