7e1f503fb5280933693ddf3b26bf01fcdaaf925e
webbread@126.com <webbread@126.com>
2017-04-11 17:51:49 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/webbread/coolweather/db/Province.java
@@ -0,0 +1,39 @@
+package com.webbread.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by webbread on 2017/4/11.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
