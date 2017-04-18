d2bedad0faf9bb5420aaaee09c2952bc425e5c72
webbread@126.com <webbread@126.com>
2017-04-18 16:48:52 +0800

    添加后台自动更新天气的功能

--- app/src/main/java/com/webbread/coolweather/WeatherActivity.java
+++ app/src/main/java/com/webbread/coolweather/WeatherActivity.java
@@ -173,43 +173,42 @@ public class WeatherActivity extends AppCompatActivity {
      * @param weather
      */
     private void showWeatherInfo(Weather weather) {
-        if (weather != null && "ok".equals(weather.status)) {
-            String cityName = weather.basic.cityName;
-            String updateTime = weather.basic.update.updateTime.split(" ")[1];
-            String degree = weather.now.temperature + "℃";
-            String weatherInfo = weather.now.more.info;
-            titleCity.setText(cityName);
-            titleUpdateTime.setText(updateTime);
-            degreeText.setText(degree);
-            weatherInfoText.setText(weatherInfo);
-            forecastLayout.removeAllViews();
-            for (Forecast forecast : weather.forecastList) {
-                View view = LayoutInflater.from(this).inflate(R.layout.forecast_item, forecastLayout, false);
-                TextView dateText = (TextView) view.findViewById(R.id.data_text);
-                TextView infoText = (TextView) view.findViewById(R.id.info_text);
-                TextView maxText = (TextView) view.findViewById(R.id.max_text);
-                TextView minText = (TextView) view.findViewById(R.id.min_text);
-                //赋值？
-                dateText.setText(forecast.date);
-                infoText.setText(forecast.more.info);
-                maxText.setText(forecast.temperature.max);
-                minText.setText(forecast.temperature.min);
-                forecastLayout.addView(view);
-            }
-            if (weather.aqi != null) {
-                aqiText.setText(weather.aqi.city.aqi);
-                pm25Text.setText(weather.aqi.city.pm25);
-            }
-            String comfort = "舒适度：" + weather.suggestion.comfort.info;
-            String carWash = "洗车指数：" + weather.suggestion.carWash.info;
-            String sport = "运动建议：" + weather.suggestion.sport.info;
-            comfortText.setText(comfort);
-            carWashText.setText(carWash);
-            sportText.setText(sport);
-            weatherLayout.setVisibility(View.VISIBLE);
-            //添加后台自动服务
-            Intent intent = new Intent(this, AutoUpdateService.class);
-            startService(intent);
+        String cityName = weather.basic.cityName;
+        String updateTime = weather.basic.update.updateTime.split(" ")[1];
+        String degree = weather.now.temperature + "℃";
+        String weatherInfo = weather.now.more.info;
+        titleCity.setText(cityName);
+        titleUpdateTime.setText(updateTime);
+        degreeText.setText(degree);
+        weatherInfoText.setText(weatherInfo);
+        forecastLayout.removeAllViews();
+        for (Forecast forecast : weather.forecastList) {
+            View view = LayoutInflater.from(this).inflate(R.layout.forecast_item, forecastLayout, false);
+            TextView dateText = (TextView) view.findViewById(R.id.data_text);
+            TextView infoText = (TextView) view.findViewById(R.id.info_text);
+            TextView maxText = (TextView) view.findViewById(R.id.max_text);
+            TextView minText = (TextView) view.findViewById(R.id.min_text);
+            //赋值？
+            dateText.setText(forecast.date);
+            infoText.setText(forecast.more.info);
+            maxText.setText(forecast.temperature.max);
+            minText.setText(forecast.temperature.min);
+            forecastLayout.addView(view);
+        }
+        if (weather.aqi != null) {
+            aqiText.setText(weather.aqi.city.aqi);
+            pm25Text.setText(weather.aqi.city.pm25);
+        }
+        String comfort = "舒适度：" + weather.suggestion.comfort.info;
+        String carWash = "洗车指数：" + weather.suggestion.carWash.info;
+        String sport = "运动建议：" + weather.suggestion.sport.info;
+        comfortText.setText(comfort);
+        carWashText.setText(carWash);
+        sportText.setText(sport);
+        weatherLayout.setVisibility(View.VISIBLE);
+        //添加后台自动服务
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
     /**
